c4e1c70e6b1cb21a1f8e0322438d53ad6e97b5b2
WNomunomu <kohsei.nmr@gmail.com>
2025-09-08 03:54:24 +0000

    Fix to enable Spring Boot application startup

--- backend/app/src/main/kotlin/com/example/taskmanager/TaskManagerApplication.kt
+++ backend/app/src/main/kotlin/com/example/taskmanager/TaskManagerApplication.kt
@@ -1,11 +1,11 @@
-package com.techtok.backend
+package com.example.taskmanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TodoApplication
+class TaskManagerApplication
 
 fun main(args: Array<String>) {
-    runApplication<TodoApplication>(*args)
+  runApplication<TaskManagerApplication>(*args)
 }
